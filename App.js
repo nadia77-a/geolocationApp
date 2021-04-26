7c3795ace7db41e2e3fbb2a8300ca7b46e388374
nadia <nadiaarapi@hotmail.com>
2021-04-26 15:57:44 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import {FlatList, View, Text} from 'react-native';
+import ForecastCard from './components/ForecastCard';
+import GetLocation from 'react-native-get-location';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      forecast: [],
+      error: '',
+    };
+  }
+
+  componentDidMount() {
+    // Get the user's location
+    this.getLocation();
+  }
+
+  getLocation() {
+    // Get the current position of the user
+
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then((location) => {
+        console.log('location', location);
+
+        this.setState({
+          latitude: location && location.latitude,
+          longitude: location && location.longitude,
+        });
+        this.getWeather();
+      })
+      .catch((error) => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     alert('position', position);
+    //     this.setState(
+    //       (prevState) => ({
+    //         latitude: position && position.coords.latitude,
+    //         longitude: position && position.coords.longitude,
+    //       }),
+    //       () => {
+    //         this.getWeather();
+    //       },
+    //     );
+    //   },
+    //   (error) => this.setState({forecast: error.message}),
+    //   {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    // );
+  }
+
+  getWeather() {
+    // Construct the API url to call
+    let url =
+      'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+      this.state.latitude +
+      '&lon=' +
+      this.state.longitude +
+      '&units=metric&appid=bef710ad45c613f6b5e559dad3cbfe75';
+
+    // Call the API, and set the state of the weather forecast
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState((prevState, props) => ({
+          forecast: data,
+        }));
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        <Text>
+          Country:{' '}
+          {this.state.forecast.city && this.state.forecast.city.country}
+        </Text>
+        <Text>
+          You are from{' '}
+          {this.state.forecast.city && this.state.forecast.city.country} that
+          has population{' '}
+          {this.state.forecast.city && this.state.forecast.city.population}
+        </Text>
+        <FlatList
+          data={this.state.forecast.list}
+          style={{marginTop: 10}}
+          keyExtractor={(item) => item.dt_txt}
+          renderItem={({item}) => (
+            <ForecastCard
+              detail={item}
+              location={this.state.forecast.city.name}
+            />
+          )}
+        />
+      </View>
+    );
+  }
+}
---
+++ components/ForecastCard.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+import {StyleSheet, View, Image} from 'react-native';
+import {Text, Card, Divider} from 'react-native-elements';
+
+class ForecastCard extends Component {
+  render() {
+    let time;
+    // Create a new date from the passed date time
+    var date = new Date(this.props.detail.dt * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = '0' + date.getMinutes();
+    time = hours + ':' + minutes.substr(-2);
+
+    return (
+      <Card containerStyle={styles.card}>
+        <Text style={styles.notes}>{this.props.location}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Image
+            style={{width: 70, height: 70}}
+            source={{
+              uri:
+                'https://openweathermap.org/img/w/' +
+                this.props.detail.weather[0].icon +
+                '.png',
+            }}
+          />
+          <Text style={styles.time}>
+            {date.getDate()}/{date.getMonth()}/{date.getFullYear()} {time}
+          </Text>
+        </View>
+        <Divider style={{backgroundColor: '#dfe6e9', marginVertical: 5}} />
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={styles.notes}>
+            {this.props.detail.weather[0].description}
+          </Text>
+          <Text style={styles.notes}>
+            {Math.round(this.props.detail.main.temp * 10) / 10}&#8451;
+          </Text>
+        </View>
+      </Card>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'rgba(56, 172, 236, 1)',
+    borderWidth: 0,
+    borderRadius: 20,
+  },
+  time: {
+    fontSize: 24,
+  },
+  notes: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+  },
+});
+
+export default ForecastCard;
