bc376c5a10524aacdd4b9446e869f79407a51099
nadia <nadiaarapi@hotmail.com>
2021-06-21 16:00:16 +0200

    feature: Add new feature

--- components/ForecastCard.js
+++ components/ForecastCard.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import {StyleSheet, View, Image} from 'react-native';
 import {Text, Card, Divider} from 'react-native-elements';
-
+//feature
 class ForecastCard extends Component {
   render() {
     //test changelog
